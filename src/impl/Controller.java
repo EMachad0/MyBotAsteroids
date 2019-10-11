1156dc57f6f8f6d2ca78e08da0ccda703c5c2bf9
Eliton <elitonmachadod200@gmail.com>
2019-10-10 23:06:48 -0300

    First changes

---
+++ src/impl/Controller.java
@@ -0,0 +1,31 @@
+package impl;
+
+import java.io.IOException;
+
+import bot_interface.Action;
+import bot_interface.BotBase;
+import bot_interface.GameState;
+
+public class Controller extends BotBase {
+
+	private int x = 0;
+	public Action process(GameState gamestate) {
+		gamestate.log(String.format("%.2f ", this.getPosy()) + this.getVely());
+		x++;
+		if (x == 5) {
+			x++;
+			return new Action(1, 0, 0, 0);
+		} else if (x == 50) {
+			x++;
+			return new Action(-1, 0, 0, 0);
+		} else return new Action(0, 0, 0, 0);
+//		if ((int) this.getPosy() > 0 && this.getVely() >= -9) return new Action(1, 0, 0, 0);
+//		if ((int) this.getPosy() < 0 && this.getVely() <= 9) return new Action(-1, 0, 0, 0);
+//		return new Action(0, 0, 0, 0);
+	}
+
+	public static void main(String[] args) throws IOException {
+		GameState game = new GameState(new StupidBot());
+		game.connect();
+	}
+}
